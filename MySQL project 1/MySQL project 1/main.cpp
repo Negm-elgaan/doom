385e46eb7869b3319d841ae8351e9380cd56782f
Negm-elgaan <ohossam962@gmail.com>
2024-12-17 06:39:38 +0200

    First C++ Database Connection

---
+++ MySQL project 1/MySQL project 1/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+using namespace std;
+
+//for demonstration only. never save your password in the code!
+const string server = "tcp://127.0.0.1:3306";
+const string username = "root";
+const string password = "Omar2003negm*";
+
+int main()
+{
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::Statement* stmt;
+    sql::PreparedStatement* pstmt;
+
+    try
+    {
+        driver = get_driver_instance();
+        con = driver->connect(server, username, password);
+    }
+    catch (sql::SQLException e)
+    {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        system("pause");
+        exit(1);
+    }
+
+    //please create database "quickstartdb" ahead of time
+    con->setSchema("CPP_DATABASE");
+
+    stmt = con->createStatement();
+    stmt->execute("DROP TABLE IF EXISTS inventory");
+    cout << "Finished dropping table (if existed)" << endl;
+    stmt->execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER , price DOUBLE);");
+    cout << "Finished creating table" << endl;
+    delete stmt;
+
+    pstmt = con->prepareStatement("INSERT INTO inventory(name, quantity, price) VALUES(?,?,?)");
+    pstmt->setString(1, "banana");
+    pstmt->setInt(2, 150);
+    pstmt->setDouble(3, 12.5);
+    pstmt->execute();
+    cout << "One row inserted." << endl;
+
+    pstmt->setString(1, "orange");
+    pstmt->setInt(2, 154);
+    pstmt->setDouble(3, 13.1343);
+    pstmt->execute();
+    cout << "One row inserted." << endl;
+
+    pstmt->setString(1, "apple");
+    pstmt->setInt(2, 100);
+    pstmt->setDouble(3, 11.7);
+    pstmt->execute();
+    cout << "One row inserted." << endl;
+
+    delete pstmt;
+    delete con;
+    system("pause");
+    return 0;
+}
