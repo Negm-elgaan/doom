7f772a0c2a6d490d8a39f3f1d49da3706a8400be
Negm-elgaan <ohossam962@gmail.com>
2025-06-18 12:31:09 +0300

    Red Black Tree v0.0.2

--- RedBlackTree/RedBlackTree/clsRedBlackTree.h
+++ RedBlackTree/RedBlackTree/clsRedBlackTree.h
@@ -21,8 +21,40 @@ template <class T> class clsRedBlackTree
 				int _Height;
 
 				friend class clsRedBlackTree;
+
+			public:
+
+				Node()
+				{
+					_Data = T();
+					_Right = nullptr;
+					_Left = nullptr;
+					_Prev = nullptr;
+					_Height = 0;
+					_Color = enColor::Red;
+				}
 		};
 
+		Node* _RootNode;
 
+	public:
+
+		clsRedBlackTree()
+		{
+			_RootNode = nullptr;
+		}
+
+		void Insert(T Data)
+		{
+
+			if (!_RootNode)
+			{
+				_RootNode = new Node();
+				_RootNode->_Data = Data;
+				_RootNode->_Height = 0; // _RootNode->_Height = 1; if rotations don't work use this  as rotations are made for min height =  1
+				_RootNode->_Color = enColor::Black;
+			}
+
+		}
 };
 
