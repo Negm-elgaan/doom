59a463ca911a63183bc7a2ecf6e4ade55e6f54cb
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2025-11-16 19:18:11 +0200

    Priority Queue v0.9
    Implement clsPriorityQueue with basic operations

--- Priority Queue/Priority Queue/clsPriorityQueue.h
+++ Priority Queue/Priority Queue/clsPriorityQueue.h
@@ -1,9 +1,87 @@
 #pragma once
+
+#include <iostream>
+#include "BinaryHeap.h"
+
+using namespace std;
+
+template <class T , class F>
+
 class clsPriorityQueue
 {
-	private:
 
-		int _Priority;
-		int _Data;
+protected:
+    MinBinaryHeap <T , F> _MyHeap;
+
+public:
+
+    void push(T Item , F Name)
+    {
+        _MyHeap.Insert(Item , Name);
+    }
+
+    void pop()
+    {
+        _MyHeap.Remove();
+    }
+
+   void Print()
+    {
+       _MyHeap.Print();
+    }
+
+    int Size()
+    {
+        return _MyHeap.Size();
+    }
+
+    bool IsEmpty()
+    {
+        return _MyHeap.IsEmpty();
+    }
+
+    T front()
+    {
+        return _MyHeap.Getitem(0);
+    }
+
+    /*T back()
+    {
+        return _MyHeap.Getitem(Size() - 1);
+    }*/
+
+   /* void Reverse()
+    {
+        _MyHeap.Reverse();
+    }
+
+    bool UpdateItem(int index , T newval)
+    {
+        return _MyHeap.UpdateItem(index, newval);
+    }*/
+
+    T GetItem(int index)
+    {
+        _MyHeap.GetItem(index);
+    }
+
+    /*void InsertAtFront(T Data)
+    {
+        _MyHeap.InsertAtBeginning(Data);
+    }
+
+    void InsertAtBack(T Data)
+    {
+        _MyHeap.InsertAtEnd(Data);
+    }*/
+
+   /* bool InsertAfter(int index, T Data)
+    {
+        return _MyHeap.InsertAfter(index, Data);
+    }*/
+
+    void Clear()
+    {
+        _MyHeap.Clear();
+    }
 };
-
