6e0782f4aefd44c6f35bf9c13a2ea1018d5c6b92
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2025-11-21 02:08:39 +0200

    Enhance ArenaAllocater with destructor management
    Added destructor allocation handling and improved destructor logic.

--- ArenaAllocatorCpp/ArenaAllocator.cpp
+++ ArenaAllocatorCpp/ArenaAllocator.cpp
@@ -1,13 +1,20 @@
 #include "ArenaAllocator.h"
 
+void* ArenaAllocater::DestructorAlloc(size_t Size)
+{
+    return ArenaAlloc(MyDestructorListArena , Size);
+}
+
 ArenaAllocater::ArenaAllocater()
 {
     MyArena = ArenaCreator(MyArena , 1024);
+    MyDestructorListArena = ArenaCreator(MyDestructorListArena , 1024);
 }
 
 ArenaAllocater::ArenaAllocater(size_t Size)
 {
     MyArena = ArenaCreator(MyArena , Size);
+    MyDestructorListArena = ArenaCreator(MyDestructorListArena , Size);
 }
 
 void* ArenaAllocater::Alloc(size_t Size)
@@ -17,5 +24,13 @@ void* ArenaAllocater::Alloc(size_t Size)
 
 ArenaAllocater::~ArenaAllocater()
 {
+    while (CurrentNode != nullptr)
+    {
+        CurrentNode->DestructorPtr(CurrentNode->Objectptr);
+        CurrentNode = CurrentNode->Prev;
+    }
+
+    ArenaDestroy(MyDestructorListArena);
+
     ArenaDestroy(MyArena);
 }
