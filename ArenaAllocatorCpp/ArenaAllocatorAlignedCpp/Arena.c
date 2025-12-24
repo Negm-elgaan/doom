f3840ba7069990ca2a9ed57fac6b6b1f62f81a27
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2025-12-24 23:45:39 +0200

    Refactor memory management in Arena allocator
    Updated memory allocation methods to use VirtualAlloc and VirtualFree for better control over memory management. Added ArenaReset function to reset the current allocation count.

--- ArenaAllocatorCpp/ArenaAllocatorAlignedCpp/Arena.c
+++ ArenaAllocatorCpp/ArenaAllocatorAlignedCpp/Arena.c
@@ -61,14 +61,20 @@ void ArenaDestroy(struct Arena* MyArena)
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdalign.h>
+#include <windows.h>
 #include "Arena.h"
 
-const size_t _Default_Capacity = 1024;
+const size_t _Default_Capacity = 4 * 1024 * 1024;
 
 
 struct Region* CreateRegion(size_t Capacity)
 {
-    struct Region* NewRegion = (struct Region *)malloc(sizeof(struct Region) + Capacity);
+    struct Region* NewRegion = (struct Region *)VirtualAlloc(NULL , (sizeof(struct Region) + Capacity) , MEM_RESERVE | MEM_COMMIT , PAGE_READWRITE);
+
+    /*if (NewRegion == NULL) {
+        return NULL; // Out of Memory!
+    }*/
+
     NewRegion->_Capacity = Capacity;
     NewRegion->_Count = 0;
     NewRegion->_Next = NULL;
@@ -78,7 +84,7 @@ struct Region* CreateRegion(size_t Capacity)
 
 struct Arena* ArenaCreator(struct Arena* MyArena , size_t Capacity)
 {
-    MyArena = (struct Arena *)malloc(sizeof(struct Arena));
+    MyArena = (struct Arena *)VirtualAlloc(NULL , sizeof(struct Arena) , MEM_RESERVE | MEM_COMMIT , PAGE_READWRITE);
     MyArena->_Start = CreateRegion(Capacity);
     MyArena->_End = MyArena->_Start;
     MyArena->_Current = MyArena->_Start;
@@ -94,7 +100,21 @@ void* ArenaAlloc(struct Arena* MyArena , size_t Capacity)
         return vptr;
     }
 
+    while (MyArena->_Current->_Next)
+    {
+        MyArena->_Current = MyArena->_Current->_Next;
+        if (MyArena->_Current->_Capacity - MyArena->_Current->_Count >= Capacity)
+        {
+            void* vptr = MyArena->_Current->_Data + MyArena->_Current->_Count;
+            MyArena->_Current->_Count += Capacity;
+            return vptr;
+        }
+    }
+
     struct Region* NewRegion = CreateRegion(Capacity < _Default_Capacity ? _Default_Capacity : Capacity);
+    /*if (NewRegion == NULL) {
+        return NULL; // Crash prevention: Return NULL instead of segfaulting
+    }*/
     MyArena->_End->_Next = NewRegion;
     NewRegion->_Prev = MyArena->_End;
     MyArena->_End = NewRegion;
@@ -130,7 +150,38 @@ void* ArenaAllocAligned(struct Arena* MyArena , size_t Capacity , size_t Alignme
         return vptr;
     }
 
+    while (MyArena->_Current->_Next)
+    {
+        MyArena->_Current = MyArena->_Current->_Next;
+        Address = (uintptr_t)MyArena->_Current->_Data + (uintptr_t)MyArena->_Current->_Count;
+        //uintptr_t Alignment = Capacity;
+        Drift = Address % Alignment;
+        padding = (Alignment - Drift) % Alignment;
+        if (MyArena->_Current->_Capacity - MyArena->_Current->_Count >= Capacity + padding)
+        {
+            void* vptr;
+            if ((Address % Alignment) != 0)
+            {
+                vptr = (void*)(Address + (Alignment - (Address % Alignment)));
+                MyArena->_Current->_Count += Capacity + (Alignment - (Address % Alignment));
+            }
+
+            else
+            {
+                vptr = (void*)Address;
+                MyArena->_Current->_Count += Capacity;
+            }
+            //void* vptr = MyArena->_Current->_Data + MyArena->_Current->_Count; //Address
+
+            vptr = (void*)vptr;
+            return vptr;
+        }
+    }
+
     struct Region* NewRegion = CreateRegion(Capacity < _Default_Capacity ? _Default_Capacity  + (Alignment - 1 ) : Capacity + (Alignment - 1 ));
+    /*if (NewRegion == NULL) {
+        return NULL; // Crash prevention: Return NULL instead of segfaulting
+    }*/
     MyArena->_End->_Next = NewRegion;
     NewRegion->_Prev = MyArena->_End;
     MyArena->_End = NewRegion;
@@ -156,6 +207,18 @@ void* ArenaAllocAligned(struct Arena* MyArena , size_t Capacity , size_t Alignme
     //return vptr;
 };
 
+void ArenaReset(struct Arena* MyArena)
+{
+    struct Region* Temp = MyArena->_Start;
+    while (Temp != MyArena->_Current)
+    {
+        Temp->_Count = 0;
+        Temp = Temp->_Next;
+    }
+    MyArena->_Current->_Count = 0;
+    MyArena->_Current = MyArena->_Start;
+}
+
 void ArenaDestroy(struct Arena* MyArena)
 {
     struct Region* Temp1 = MyArena->_Start;
@@ -163,11 +226,11 @@ void ArenaDestroy(struct Arena* MyArena)
     while(Temp1 != NULL)
     {
         struct Region* Temp2 = Temp1->_Next;
-        free(Temp1);
+        VirtualFree(Temp1 , 0 , MEM_RELEASE);
         Temp1 = Temp2;
     }
 
-    free(MyArena);
+    VirtualFree(MyArena , 0 , MEM_RELEASE);
 
     return;
 }
