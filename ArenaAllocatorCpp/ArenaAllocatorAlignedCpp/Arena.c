f3ae6de5ce9c48d9fdbfeabd019ed7a79c7f6550
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2025-12-29 03:31:20 +0200

    Refactor Arena memory management with VirtualAlloc
    Refactor memory allocation to use VirtualAlloc instead of malloc. Added alignment support in ArenaAllocAligned and tracking for total bytes used.

--- ArenaAllocatorCpp/ArenaAllocatorAlignedCpp/Arena.c
+++ ArenaAllocatorCpp/ArenaAllocatorAlignedCpp/Arena.c
@@ -66,7 +66,6 @@ void ArenaDestroy(struct Arena* MyArena)
 
 const size_t _Default_Capacity = 4 * 1024 * 1024;
 
-
 struct Region* CreateRegion(size_t Capacity)
 {
     struct Region* NewRegion = (struct Region *)VirtualAlloc(NULL , (sizeof(struct Region) + Capacity) , MEM_RESERVE | MEM_COMMIT , PAGE_READWRITE);
@@ -88,6 +87,7 @@ struct Arena* ArenaCreator(struct Arena* MyArena , size_t Capacity)
     MyArena->_Start = CreateRegion(Capacity);
     MyArena->_End = MyArena->_Start;
     MyArena->_Current = MyArena->_Start;
+    MyArena->_Total_Bytes_Used = 0;
     return MyArena;
 };
 
@@ -97,6 +97,7 @@ void* ArenaAlloc(struct Arena* MyArena , size_t Capacity)
     {
         void* vptr = MyArena->_Current->_Data + MyArena->_Current->_Count;
         MyArena->_Current->_Count += Capacity;
+        MyArena->_Total_Bytes_Used += Capacity;
         return vptr;
     }
 
@@ -107,6 +108,7 @@ void* ArenaAlloc(struct Arena* MyArena , size_t Capacity)
         {
             void* vptr = MyArena->_Current->_Data + MyArena->_Current->_Count;
             MyArena->_Current->_Count += Capacity;
+            MyArena->_Total_Bytes_Used += Capacity;
             return vptr;
         }
     }
@@ -121,6 +123,7 @@ void* ArenaAlloc(struct Arena* MyArena , size_t Capacity)
     MyArena->_Current = NewRegion;
     void* vptr = MyArena->_Current->_Data + MyArena->_Current->_Count;
     MyArena->_Current->_Count += Capacity;
+    MyArena->_Total_Bytes_Used += Capacity;
     return vptr;
 };
 
@@ -137,12 +140,14 @@ void* ArenaAllocAligned(struct Arena* MyArena , size_t Capacity , size_t Alignme
         {
             vptr = (void*)(Address + (Alignment - (Address % Alignment)));
             MyArena->_Current->_Count += Capacity + (Alignment - (Address % Alignment));
+            MyArena->_Total_Bytes_Used += Capacity + (Alignment - (Address % Alignment));
         }
 
         else
         {
             vptr = (void*)Address;
             MyArena->_Current->_Count += Capacity;
+            MyArena->_Total_Bytes_Used += Capacity;
         }
         //void* vptr = MyArena->_Current->_Data + MyArena->_Current->_Count; //Address
 
@@ -164,12 +169,14 @@ void* ArenaAllocAligned(struct Arena* MyArena , size_t Capacity , size_t Alignme
             {
                 vptr = (void*)(Address + (Alignment - (Address % Alignment)));
                 MyArena->_Current->_Count += Capacity + (Alignment - (Address % Alignment));
+                MyArena->_Total_Bytes_Used += Capacity + (Alignment - (Address % Alignment));
             }
 
             else
             {
                 vptr = (void*)Address;
                 MyArena->_Current->_Count += Capacity;
+                MyArena->_Total_Bytes_Used += Capacity;
             }
             //void* vptr = MyArena->_Current->_Data + MyArena->_Current->_Count; //Address
 
@@ -193,12 +200,14 @@ void* ArenaAllocAligned(struct Arena* MyArena , size_t Capacity , size_t Alignme
     {
         vptr = Address + (Alignment - (Address % Alignment));
         MyArena->_Current->_Count += Capacity + (Alignment - (Address % Alignment));
+        MyArena->_Total_Bytes_Used += Capacity + (Alignment - (Address % Alignment));
     }
 
     else
     {
         vptr = (void*)Address;
         MyArena->_Current->_Count += Capacity;
+        MyArena->_Total_Bytes_Used += Capacity;
     }
         //void* vptr = MyArena->_Current->_Data + MyArena->_Current->_Count; //Address
     return vptr;
@@ -206,6 +215,11 @@ void* ArenaAllocAligned(struct Arena* MyArena , size_t Capacity , size_t Alignme
     //MyArena->_Current->_Count += Capacity;
     //return vptr;
 };
+
+size_t BytesUsed(struct Arena* MyArena)
+{
+    return MyArena->_Total_Bytes_Used;
+}
 
 void ArenaReset(struct Arena* MyArena)
 {
@@ -217,6 +231,7 @@ void ArenaReset(struct Arena* MyArena)
     }
     MyArena->_Current->_Count = 0;
     MyArena->_Current = MyArena->_Start;
+    MyArena->_Total_Bytes_Used = 0;
 }
 
 void ArenaDestroy(struct Arena* MyArena)
