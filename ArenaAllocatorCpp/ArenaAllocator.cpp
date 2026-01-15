38fd53da9ab1e84a52725282f3740388e7d81297
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2026-01-15 19:33:35 +0200

    minor bug fixes

--- ArenaAllocatorCpp/ArenaAllocator.cpp
+++ ArenaAllocatorCpp/ArenaAllocator.cpp
@@ -5,21 +5,118 @@ void* ArenaAllocater::DestructorAlloc(size_t Size)
     return ArenaAlloc(MyDestructorListArena , Size);
 }
 
+void* ArenaAllocater::DestructorAllocAligned(size_t Size , size_t Alignment)
+{
+    return ArenaAllocAligned(MyDestructorListArena , Size , Alignment);
+}
+
 ArenaAllocater::ArenaAllocater()
 {
-    MyArena = ArenaCreator(MyArena , 1024);
-    MyDestructorListArena = ArenaCreator(MyDestructorListArena , 1024);
+    MyArena = ArenaCreator(1024);
+    MyDestructorListArena = ArenaCreator(1024);
 }
 
 ArenaAllocater::ArenaAllocater(size_t Size)
 {
-    MyArena = ArenaCreator(MyArena , Size);
-    MyDestructorListArena = ArenaCreator(MyDestructorListArena , Size);
+    MyArena = ArenaCreator(Size);
+    MyDestructorListArena = ArenaCreator(Size);
 }
 
 void* ArenaAllocater::Alloc(size_t Size)
 {
     return ArenaAlloc(MyArena , Size);
+}
+
+void* ArenaAllocater::Alloc(size_t Size , size_t Alignment)
+{
+    return ArenaAllocAligned(MyArena , Size , Alignment);
+}
+
+void* ArenaAllocater::MonoAlloc(size_t Size , size_t Alignment)
+{
+    return MonotonicArenaAllocAligned(MyArena , Size , Alignment);
+}
+
+ArenaSnap* ArenaAllocater::SnapShot()
+{
+    ArenaSnap* ArenaSnapper = new ArenaSnap{};
+    ArenaSnapper->Snappy = Snap(MyArena);
+    ArenaSnapper->DestructorArenaSnappy = Snap(MyDestructorListArena);
+    ArenaSnapper->SnapDestructorNode = CurrentNode;
+    return ArenaSnapper;
+}
+
+ArenaSnap ArenaAllocater::SnapShotByValue()
+{
+    ArenaSnap ArenaSnapper;
+    ArenaSnapper.Snappy = Snap(MyArena);
+    ArenaSnapper.DestructorArenaSnappy = Snap(MyDestructorListArena);
+    ArenaSnapper.SnapDestructorNode = CurrentNode;
+    return ArenaSnapper;
+}
+
+void ArenaAllocater::Rewinder(ArenaSnap* Snap)
+{
+    while (CurrentNode != Snap->SnapDestructorNode)
+    {
+        CurrentNode->DestructorPtr(CurrentNode->Objectptr);
+        CurrentNode = CurrentNode->Prev;
+    }
+    MyArena = Rewind(MyArena , Snap->Snappy);
+    MyDestructorListArena = Rewind(MyDestructorListArena , Snap->DestructorArenaSnappy);
+    MY_FREE(Snap->Snappy, sizeof(Arena_Snap));
+    MY_FREE(Snap->DestructorArenaSnappy, sizeof(Arena_Snap));
+    delete Snap;
+}
+
+void ArenaAllocater::RewinderByValue(ArenaSnap Snap)
+{
+    while (CurrentNode != Snap.SnapDestructorNode)
+    {
+        CurrentNode->DestructorPtr(CurrentNode->Objectptr);
+        CurrentNode = CurrentNode->Prev;
+    }
+    MyArena = Rewind(MyArena , Snap.Snappy);
+    MyDestructorListArena = Rewind(MyDestructorListArena , Snap.DestructorArenaSnappy);
+    MY_FREE(Snap.Snappy, sizeof(Arena_Snap));
+    MY_FREE(Snap.DestructorArenaSnappy, sizeof(Arena_Snap));
+}
+
+size_t ArenaAllocater::ByteUse()
+{
+    return BytesUsed(MyArena);
+}
+
+size_t ArenaAllocater::BytesAllocated()
+{
+    return BytesAlloc(MyArena);
+}
+
+size_t ArenaAllocater::TotalBytesAllocated()
+{
+    return TotalBytes(MyArena);
+}
+
+size_t ArenaAllocater::FreeSpace()
+{
+    return FreeBytes(MyArena);
+}
+
+bool ArenaAllocater::IsEmpty()
+{
+    return Isempty(MyArena);
+}
+
+void ArenaAllocater::ArenaAllocaterReset()
+{
+    while (CurrentNode != nullptr)
+    {
+        CurrentNode->DestructorPtr(CurrentNode->Objectptr);
+        CurrentNode = CurrentNode->Prev;
+    }
+
+    ArenaReset(MyDestructorListArena);
+    ArenaReset(MyArena);
 }
 
 ArenaAllocater::~ArenaAllocater()
