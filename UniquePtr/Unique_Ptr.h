ae9efe9e4c5417460372ce7f2b8c952261157efa
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2026-01-15 09:45:32 +0200

    Create UniquePtr/Unique_Ptr.h

---
+++ UniquePtr/Unique_Ptr.h
@@ -0,0 +1,10 @@
+template  Unique_Ptr { T* ptr; public: //Some functions and will add move constructor and move assignment operator
+Unique_Ptr()
+{
+ptr = nullptr;
+}
+Unique_Ptr(const Unique_Ptr& other) = delete;
+
+Unique_Ptr& operator=(Unique_Ptr& other) = delete;
+
+~Unique_Ptr() { delete ptr; } }
