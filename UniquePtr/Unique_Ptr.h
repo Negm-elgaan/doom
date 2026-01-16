472e2a7762f86a538fe8f0ee62107e35b5f1333d
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2026-01-16 21:04:13 +0200

    UniquePtr V1.1

--- UniquePtr/Unique_Ptr.h
+++ UniquePtr/Unique_Ptr.h
@@ -1,33 +1,50 @@
-template <class T> class Unique_Ptr 
-{ 
-T* ptr; 
-public: //Some functions will be added
-Unique_Ptr()
-{
-ptr = nullptr;
-}
-Unique_Ptr(T* Data)
-{
-ptr = Data;
-}
-Unique_Ptr(Unique_Ptr&& other)
-{
-this->ptr = other.ptr;
-other.ptr = nullptr;
-}
-Unique_Ptr& operator=(Unique_Ptr&& other)
-{
-if(this != &other)
-{
-delete this->ptr;
-this->ptr = other.ptr;
-other.ptr = nullptr;
-}
-return *this;
-}
-Unique_Ptr(const Unique_Ptr& other) = delete;
+#include <iostream>
 
-Unique_Ptr& operator=(Unique_Ptr& other) = delete;
+template <class T> class Unique_Ptr
+{
+    T* ptr;
+    public: //Some functions will be added
 
-~Unique_Ptr() { delete ptr; } 
+        Unique_Ptr()
+        {
+            ptr = nullptr;
+        }
+
+        explicit Unique_Ptr(T* Data)
+        {
+            ptr = Data;
+        }
+
+        Unique_Ptr(Unique_Ptr&& other) noexcept
+        {
+            this->ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+
+        Unique_Ptr& operator=(Unique_Ptr&& other) noexcept
+        {
+            if(this != &other)
+            {
+                delete this->ptr;
+                this->ptr = other.ptr;
+                other.ptr = nullptr;
+            }
+            return *this;
+        }
+
+        T& operator*() const //returns a reference  which is an alias for the original variable
+        {
+            return *ptr;
+        }
+
+        T* operator->() const noexcept //returns a pointer which holds the memory address
+        {
+            return ptr;
+        }
+
+        Unique_Ptr(const Unique_Ptr& other) = delete;
+
+        Unique_Ptr& operator=(const Unique_Ptr& other) = delete;
+
+        ~Unique_Ptr() { delete ptr; }
 };
