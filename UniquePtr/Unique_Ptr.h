a3765dbfe6efa4aef8f8f02e52dcc575f9362240
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2026-01-20 19:49:40 +0200

    Added Array specialization and release function

--- UniquePtr/Unique_Ptr.h
+++ UniquePtr/Unique_Ptr.h
@@ -43,26 +43,101 @@ template <class T> class Unique_Ptr
         {
             return ptr;
         }
-        
+
         explicit operator bool() const noexcept
         {
             return ptr != nullptr;
+        }
+        
+        T* Release() noexcept
+        {
+            T* Releaser = ptr;
+            ptr = nullptr;
+            return Releaser;
         }
         
         T* Get() const noexcept
         {
             return ptr;
         }
-        
+
         void Reset(T* newptr = nullptr)
         {
             delete ptr;
             ptr = newptr;
         }
-        
+
         Unique_Ptr(const Unique_Ptr& other) = delete;
 
         Unique_Ptr& operator=(const Unique_Ptr& other) = delete;
 
         ~Unique_Ptr() { delete ptr; }
+};
+
+template <class T> class Unique_Ptr<T[]>
+{
+    T* ptr;
+
+    public:
+
+        Unique_Ptr()
+        {
+            ptr = nullptr;
+        }
+
+        explicit Unique_Ptr(T* Data)
+        {
+            ptr = Data;
+        }
+
+        Unique_Ptr(Unique_Ptr&& other) noexcept
+        {
+            this->ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+
+        Unique_Ptr& operator=(Unique_Ptr&& other) noexcept
+        {
+            if(this != &other)
+            {
+                delete[] this->ptr;
+                this->ptr = other.ptr;
+                other.ptr = nullptr;
+            }
+            return *this;
+        }
+
+        T& operator[](int index) const
+        {
+            return ptr[index];
+        }
+
+        explicit operator bool() const noexcept
+        {
+            return ptr != nullptr;
+        }
+
+        T* Release()
+        {
+            T* Releaser = ptr;
+            ptr = nullptr;
+            return Releaser;
+        }
+        
+        T* Get() const noexcept
+        {
+            return ptr;
+        }
+
+        void Reset(T* newptr = nullptr)
+        {
+            delete[] ptr;
+            ptr = newptr;
+        }
+
+        Unique_Ptr(const Unique_Ptr& other) = delete;
+
+        Unique_Ptr& operator=(const Unique_Ptr& other) = delete;
+
+        ~Unique_Ptr() { delete[] ptr; }
 };
